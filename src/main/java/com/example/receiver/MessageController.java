0fee23ca06c599466ae3893846ebb0773babf86f
Maciej Stasinski <m.stasinski1@gmail.com>
2021-12-24 14:39:10 +0100

    Initial commit

---
+++ src/main/java/com/example/receiver/MessageController.java
@@ -0,0 +1,32 @@
+package com.example.receiver;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public MessageController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @GetMapping("/notification")
+    public ResponseEntity<Notification> receiveNotification (){
+        Notification notification = rabbitTemplate.receiveAndConvert("Kurs", ParameterizedTypeReference.forType(Notification.class));
+        if (notification != null){
+            return  ResponseEntity.ok( notification);
+        } return ResponseEntity.noContent().build(); // - nie ma żadnej wiadomości
+    }
+
+    @RabbitListener(queues = "Kurs")
+    public void listenerMessage(Notification notification){
+        System.out.println(notification.getEmail()+ " "+ notification.getTitle()+
+                " "+ notification.getBody());
+    }
+}
